f216b9b46acc4d406bc588d8d8bf5c75fb677369
er3n <erenozturk@gmail.com>
2018-11-15 22:40:37 +0300

    lock file removed

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ var express = require('express')
 var getImages = require('./getImages.js')
 
 var app = express()
-const port = 3000
+const port = process.env.PORT ? process.env.PORT : 3000
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
